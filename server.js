183b60180786acafe126f62a79b080c3a881fdad
jifrivly <jifrivly@gmail.com>
2019-04-10 22:54:54 +0530

    changed

--- server.js
+++ server.js
@@ -10,15 +10,14 @@ my_app.post('/login', (req, res) => {
     var user = req.body.username
     var pass = req.body.password
 
-    res.send(user)
-    // if (user == "admin" && pass == 1234) {
-    //     console.log("Success..")
-    //     res.send(JSON.stringify({status:"Success"}))
-    // } else {
-    //     console.log("Failed..")
-    //     res.send(JSON.stringify({status:"Failed"}))
-    // }
-    // console.log("Working")
+    if (user == "admin" && pass == 1234) {
+        console.log("Success..")
+        res.send(JSON.stringify({status:"Success"}))
+    } else {
+        console.log("Failed..")
+        res.send(JSON.stringify({status:"Failed"}))
+    }
+    console.log("Working")
 })
 
 my_app.post("/register",(req,res)=>{
